955f48548e9f3a256b4fb0501c32b386b1d84d09
John Källén <johnkal@gmail.com>
2019-10-29 15:34:52 +0100

    Feature: new swapw tool for swapping 32-bit endianness.

---
+++ swapw/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace swapw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Stream stmOut = Console.OpenStandardOutput())
+            using (Stream stmIn = Console.OpenStandardInput())
+            {
+                var rdr = new BinaryReader(stmIn);
+                    var w = new BinaryWriter(stmOut);
+                try
+                {
+                    for (; ; )
+                    {
+                        var u = rdr.ReadUInt32();
+                        var swappedU =
+                            (u >> 24) |
+                            ((u >> 8) & 0xFF00) |
+                            ((u << 8) & 0xFF0000) |
+                            ((u << 24));
+                        w.Write(swappedU);
+                    }
+                }
+                catch
+                {
+                    w.Flush();
+                }
+            }
+        }
+    }
+}
