a1c433d167a91db0318982ee91dd2bfebce5eb17
John Källén <johnkal@gmail.com>
2021-08-18 17:58:01 +0200

    New project: Benchmarks

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Reko.Core;
+using Reko.Core.Machine;
+using Reko.Core.Memory;
+using Reko.Core.Rtl;
+using Reko.Core.Services;
+
+namespace Reko.Benchmarks
+{
+    [DisassemblyDiagnoser(maxDepth: 1)] // change to 0 for just the [Benchmark] method
+    [MemoryDiagnoser(displayGenColumns: false)]
+    public class Program
+    {
+        private readonly byte[] data;
+        private readonly ByteMemoryArea mem;
+        private readonly ServiceContainer sc;
+        private readonly IProcessorArchitecture archX86;
+        private readonly IProcessorArchitecture archArm;
+        private ulong sum;
+
+        public static void Main(string[] args) =>
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, DefaultConfig.Instance
+                //.WithSummaryStyle(new SummaryStyle(CultureInfo.InvariantCulture, printUnitsInHeader: false, SizeUnit.B, TimeUnit.Microsecond))
+                );
+
+        public Program()
+        {
+            this.data = new byte[1_000_000];
+            var rnd = new Random(42);
+            rnd.NextBytes(data);
+            this.mem = new ByteMemoryArea(Address.Ptr32(0x10_0000), data);
+
+            this.sc = new ServiceContainer();
+            sc.AddService<IFileSystemService>(new FileSystemServiceImpl());
+            sc.AddService<ITestGenerationService>(new TestGenerationService(sc));
+            var options = new Dictionary<string, object>();
+            this.archX86 = new Reko.Arch.X86.X86ArchitectureFlat32(sc, "", options);
+            this.archArm = new Reko.Arch.Arm.Arm32Architecture(sc, "", options);
+
+        }
+
+        private IEnumerable<MachineInstruction> CreateDisassembler<T>() where T : IProcessorArchitecture
+        {
+            var arch = (T?)Activator.CreateInstance(typeof(T), null, "x86", new Dictionary<string, object>());
+            var rdr = arch!.CreateImageReader(mem, 0);
+            var dasm = arch.CreateDisassembler(rdr);
+            return dasm;
+        }
+
+        // BENCHMARKS GO HERE
+        //[Benchmark]
+        public void ReadBeUints()
+        {
+            this.sum = 0;
+            for (int i = 0; i < data.Length; i += 4)
+            {
+                sum += Reko.Core.Memory.ByteMemoryArea.ReadBeUInt32(data, i);
+            }
+        }
+
+        //[Benchmark]
+        public void ReadBeUintBuffer()
+        {
+            this.sum = 0;
+            var seq = new ReadOnlySpan<byte>(data);
+            for (int i = 0; i < data.Length; i += 4)
+            {
+                var rspan = seq.Slice(i);
+                sum += System.Buffers.Binary.BinaryPrimitives.ReadUInt32BigEndian(rspan);
+            }
+        }
+
+        // [Benchmark]
+        public void DisassembleX86Code()
+        {
+            this.sum = 0;
+            var dasm = MakeDasm(archX86);
+            foreach (var instr in dasm)
+            {
+                sum += (uint)(instr.Length + instr.Operands.Length);
+            }
+        }
+
+        // [Benchmark]
+        public void DisassembleArmCode()
+        {
+            this.sum = 0;
+            var dasm = MakeDasm(archArm);
+            foreach (var instr in dasm)
+            {
+                sum += (uint)(instr.Length + instr.Operands.Length);
+            }
+        }
+
+        private IEnumerable<MachineInstruction> MakeDasm(IProcessorArchitecture arch)
+        {
+            var rdr = arch.CreateImageReader(mem, 0);
+            return arch.CreateDisassembler(rdr);
+        }
+
+        
+        private IEnumerable<RtlInstructionCluster> MakeRewriter(IProcessorArchitecture arch)
+        {
+            var rdr = arch.CreateImageReader(mem, 0);
+            var rw = arch.CreateRewriter(rdr, arch.CreateProcessorState(), new StorageBinder(), new RewriterHost(arch));
+            return rw;
+        }
+
+        [Benchmark]
+        public void RewriteX86Code()
+        {
+            this.sum = 0;
+            var rw = MakeRewriter(archX86);
+            foreach (var instr in rw)
+            {
+                sum += (uint)(instr.Length);
+            }
+        }
+
+        // [Benchmark]
+        public void RewriteArmCode()
+        {
+            this.sum = 0;
+            var rw = MakeRewriter(archArm);
+            foreach (var instr in rw)
+            {
+                sum += (uint)(instr.Length);
+            }
+        }
+
+        [Benchmark]
+        public void ShingleScanner()
+        {
+            var platform = new DefaultPlatform(this.sc, archX86);
+            var segmentMap = new SegmentMap(new ImageSegment("code", mem.BaseAddress, mem, AccessMode.ReadExecute));
+            var program = new Reko.Core.Program(segmentMap, archX86, platform);
+            var eventListener = new NullDecompilerEventListener();
+            sc.AddService<DecompilerEventListener>(eventListener);
+            var scanner = new Reko.Scanning.ScannerInLinq(sc, program, new RewriterHost(program.Architecture), eventListener);
+            var sr = new Scanning.ScanResults();
+            scanner.ScanImage(sr);
+        }
+    }
+}
---
+++ Benchmarks/RewriterHost.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using Reko.Core;
+using Reko.Core.Expressions;
+using Reko.Core.Serialization;
+using Reko.Core.Types;
+
+namespace Reko.Benchmarks
+{
+    public class RewriterHost : IRewriterHost
+    {
+        private readonly IProcessorArchitecture arch;
+
+        public RewriterHost(IProcessorArchitecture arch)
+        {
+            this.arch = arch;
+        }
+
+        private readonly ConcurrentDictionary<string, ConcurrentDictionary<FunctionType, IntrinsicProcedure>> intrinsics = new();
+
+        public Expression CallIntrinsic(string name, bool isIdempotent, FunctionType fnType, params Expression[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IntrinsicProcedure EnsureIntrinsic(string name, bool isIdempotent, DataType returnType, int arity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Error(Address address, string format, params object[] args)
+        {
+            // Console.WriteLine("Error: {0}", string.Format(format, args));
+        }
+
+        public IProcessorArchitecture GetArchitecture(string archMoniker)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Expression? GetImport(Address addrThunk, Address addrInstr)
+        {
+            return null;
+        }
+
+        public ExternalProcedure? GetImportedProcedure(IProcessorArchitecture arch, Address addrThunk, Address addrInstr)
+        {
+            return null;
+        }
+
+        public ExternalProcedure GetInterceptedCall(IProcessorArchitecture arch, Address addrImportThunk)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Expression Intrinsic(string name, bool isIdempotent, DataType returnType, params Expression[] args)
+        {
+            static Identifier IdFromExpression(Expression arg, int i)
+            {
+                var id = arg as Identifier;
+                var stg = id?.Storage;
+                return new Identifier("", arg.DataType, stg!);
+            }
+
+            var sig = new FunctionType(
+                new Identifier("", returnType, null!),
+                args.Select((arg, i) => IdFromExpression(arg, i)).ToArray());
+            var intrinsic = EnsureIntrinsicProcedure(name, isIdempotent, sig);
+            return new Application(
+                new ProcedureConstant(arch.PointerType, intrinsic),
+                returnType,
+                args);
+        }
+
+        public IntrinsicProcedure EnsureIntrinsicProcedure(string name, bool isIdempotent, FunctionType sig)
+        {
+            if (!intrinsics.TryGetValue(name, out var de))
+            {
+                de = new ConcurrentDictionary<FunctionType, IntrinsicProcedure>(new DataTypeComparer());
+                if (!intrinsics.TryAdd(name, de))
+                    de = intrinsics[name];
+            }
+            if (!de.TryGetValue(sig, out var intrinsic))
+            {
+                intrinsic = new IntrinsicProcedure(name, isIdempotent, sig);
+                if (!de.TryAdd(sig, intrinsic))
+                    intrinsic = de[sig];
+            }
+            return intrinsic;
+        }
+
+        public Expression Intrinsic(string name, bool isIdempotent, ProcedureCharacteristics c, DataType returnType, params Expression[] args)
+        {
+            static Identifier IdFromExpression(Expression arg, int i)
+            {
+                var id = arg as Identifier;
+                var stg = id?.Storage;
+                return new Identifier("", arg.DataType, stg!);
+            }
+
+            var sig = new FunctionType(
+                new Identifier("", returnType, null!),
+                args.Select((arg, i) => IdFromExpression(arg, i)).ToArray());
+            var intrinsic = EnsureIntrinsicProcedure(name, isIdempotent, sig);
+            intrinsic.Characteristics = c;
+            return new Application(
+                new ProcedureConstant(arch.PointerType, intrinsic),
+                returnType,
+                args);        
+        }
+
+        public bool TryRead(IProcessorArchitecture arch, Address addr, PrimitiveType dt, out Constant value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Warn(Address address, string format, params object[] args)
+        {
+            // Console.WriteLine("Warning: {0}", string.Format(format, args));
+        }
+    }
+
+}
