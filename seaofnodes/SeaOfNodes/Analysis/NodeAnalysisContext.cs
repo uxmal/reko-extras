db67a009601c86a2d6a674f949de39a986a2440c
John Källén <johnkal@gmail.com>
2026-08-17 15:08:07 +0200

    seaofnodes: add NodeAnalysisContext

---
+++ seaofnodes/SeaOfNodes/Analysis/NodeAnalysisContext.cs
@@ -0,0 +1,26 @@
+﻿using Reko.Core;
+using Reko.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reko.Extras.SeaOfNodes.Analysis;
+
+public class NodeAnalysisContext
+{
+    public NodeAnalysisContext(
+        IReadOnlyProgram program,
+        PeepholeOptimizer peepholeOptimizer,
+        IEventListener listener)
+    {
+        this.Program = program;
+        this.PeepholeOptimizer = peepholeOptimizer;
+        this.EventListener = listener;
+    }
+
+    public IReadOnlyProgram Program { get; }
+    public PeepholeOptimizer PeepholeOptimizer { get; }
+    public IEventListener EventListener { get; }
+}
